a75be5be0690d0f1231743fd0e3be2729343b6d9
Jagannath Vaikuntham <vaikunthaj@ip-10-101-37-125.ap-southeast-2.compute.internal>
2017-12-07 12:48:57 +1100

    cleanup

--- local.conf.js
+++ local.conf.js
@@ -3,16 +3,11 @@ var  config = {
 	,seleniumAddress: 'http://localhost:4444/wd/hub'
 	, specs: ['e2e/*-spec.js']
 	, capabilities:{
-		// browserName: 'phantomjs',
-		// 'phantomjs.binary.path': require('phantomjs').path,
-		// 'phantomjs.cli.args': ['--ignore-ssl-errors=true',  '--web-security=false'],
 		browserName: 'chrome',
-		name: 'QA Meetup Job',
+		name: 'Demo Job',
 		shardTestFiles: true,
 		maxInstances: 2
 	}
-	// ,  chromeDriver: 'node_modules/chromedriver/bin/chromedriver'
-	// ,  directConnect: true
 };
 
-module.exports.config = exports.config = config;
+exports.config = config;
