67c736f151fa838813e0718960bab7043a0269e6
Jagannath Vaikuntham <vaikunthaj@ip-10-101-37-125.ap-southeast-2.compute.internal>
2017-12-07 13:52:06 +1100

    clean up

--- config/chrome-headless.conf.js
+++ config/chrome-headless.conf.js
@@ -1,12 +1,14 @@
 var  config = {
 	framework:'jasmine2'
-	,seleniumAddress: 'http://localhost:4444/wd/hub'
-	, specs: ['e2e/*-spec.js']
+	, specs: ['../e2e/*-spec.js']
 	, capabilities:{
 		browserName: 'chrome',
 		name: 'Demo Job',
 		shardTestFiles: true,
-		maxInstances: 2
+        maxInstances: 2,
+		chromeOptions: {
+ 			 args: [ "--headless", "--disable-gpu", "--window-size=800x600" ]
+        }	
 	}
 };
 
