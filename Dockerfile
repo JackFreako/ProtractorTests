87a5a8656a48a1945886bf951c93d0399e2649f2
Jagannath Vaikuntham <vaikunthaj@ip-10-101-37-125.ap-southeast-2.compute.internal>
2017-12-07 12:45:22 +1100

    Experiments

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
-
 FROM node:9.2.0-slim as code
 COPY . /code/
 WORKDIR /code/
--- local.conf.js
+++ local.conf.js
@@ -1,16 +1,27 @@
 var  config = {
 	framework:'jasmine2'
-	,seleniumAddress: 'http://localhost:4444/wd/hub'
+	// ,seleniumAddress: 'http://localhost:4444/wd/hub'
 	, specs: ['e2e/*-spec.js']
-	, capabilities:{
-		// browserName: 'phantomjs',
-		// 'phantomjs.binary.path': require('phantomjs').path,
-		// 'phantomjs.cli.args': ['--ignore-ssl-errors=true',  '--web-security=false'],
+	// , capabilities:{
+	// 	// browserName: 'phantomjs',
+	// 	// 'phantomjs.binary.path': require('phantomjs').path,
+	// 	// 'phantomjs.cli.args': ['--ignore-ssl-errors=true',  '--web-security=false'],
+	// 	// chromeOptions: {
+	// 	// 	args: [ "--headless", "--disable-gpu", "--window-size=800x600" ]
+	// 	//   },
+	// 	browserName: 'chrome',
+	// 	name: 'QA Meetup Job',
+	// 	shardTestFiles: true,
+	// 	maxInstances: 2
+	// }
+	,  capabilities:{
 		browserName: 'chrome',
-		name: 'QA Meetup Job',
+		chromeOptions: {
+			   args: [ "--headless", "--disable-gpu", "--window-size=800x600" ]
+		},
 		shardTestFiles: true,
-		maxInstances: 2
-	}
+		maxInstances: 3	
+	  }
 	// ,  chromeDriver: 'node_modules/chromedriver/bin/chromedriver'
 	// ,  directConnect: true
 };
